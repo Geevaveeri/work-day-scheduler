89a03ae79fcfdd70cf26288ecd89cbd5f831b0bb
Geeva veeri <cbr_me@yahoo.com>
2021-04-10 19:46:41 -0600

    completed feature to click and edit tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,62 +1,83 @@
 var currentDate = moment().format("dddd, MMMM Do YYYY");
-var taskSection
 var tasks = {};
-var currentTime = parseInt(moment().format("h"));
+var currentTime = parseInt(moment().format("H"));
 
 // Function to keep date correct
 $("#currentDay").text(currentDate);
 
 // checks current date and event times ever 30 minutes
 setInterval(function () {
-
+    auditTasksItem();
     $("#currentDay").text(currentDate);
-    $(".row").attr("id").each(function () {
-        auditTasks($(this));
-    })
 
-}, 1800000);
+}, 500000);
 
 // function to save tasks to local storage
 var saveTasks = function () {
     localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
-// function to check tasks against current time
-var auditTasks = function () {
-    // get date from the task element
-    var date = $(".row")
-        .attr("id")
+var loadTasks = function () {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
 
-    // remove old classes from element
-    $(".event-secion").removeClass("past present future");
+    if (!tasks) {
+        tasks = {};
+    }
 
-    if (date === currentTime) {
-        $(".event-section").addClass("present");
-    }
-    else if (date < currentTime) {
-        $(".event-section").addClass("past");
-    }
-    else if (date > currentTime) {
-        $(".event-section").addClass("future");
-    }
+    $.each(tasks, function(list, arr){
+        arr.forEach(function(task){
+            createTasks(task.text, task.time);
+        })
+    })
+
 };
-auditTasks();
+
+var createTasks = function(taskText, taskTime){
+    var taskEl = $("<span>").addClass("tast-text").text(taskText);
+
+    $("#" + taskTime).append(taskEl);
+};
+
+// function to check tasks against current time
+var auditTasksTime = function () {
+
+    $(".event-section").each(function (i) {
+        var date = $(this).attr("id");
+
+
+        // remove old classes from element
+        $(".event-secion").removeClass("past present future");
+
+        if (date === currentTime) {
+            $(this).addClass("present");
+        }
+        else if (date < currentTime) {
+            $(this).addClass("past");
+        }
+        else if (date > currentTime) {
+            $(this).addClass("future");
+        }
+
+    })
+
+};
 
 // task text was clicked
-$(".event-section").on("click","span", function () {
-    //  get current text of span element
+$(".event-section").on("click", function () {
+    //  get current text of element
     var text = $(this)
         .text()
         .trim();
 
+
     // replace span element with a new textarea
-    var textInput = $("<textarea>").addClass("textarea").attr("cols", "75").val(text);
-    $(this).replaceWith(textInput);
+    var textInput = $("<textarea>").addClass("textarea").attr("cols", "75").attr("rows", "1").val(text);
+
+    $(this).append(textInput);
 
     // auto focus new element
     textInput.trigger("focus");
-    console.log(text);
-    debugger
+
 });
 
 // save button was clicked
@@ -66,18 +87,22 @@ $(".saveBtn").on("click", function () {
 
     // get position in calander
     var status = $(this)
-        .closest(".row")
+        // .closest(".event-section")
         .attr("id")
 
     // update task in array and re-save
     tasks[status] = text;
 
-    // recreate span
+    saveTasks();
+    // recreate p
     var taskSpan = $("<span>")
         .addClass("task-text")
         .text(text);
 
     // replace textarea with new content
     $(".textarea").replaceWith(taskSpan);
-})
+});
+
+// loadTasks();
+auditTasksTime();
 console.log(tasks);
